505af11520876fc3a7b0df26ee1340c8a88615a9
Hamayama <hamay1010@gmail.com>
2014-08-29 05:05:52 +0900

    src chg v1.05

---
+++ test1001.scm
@@ -0,0 +1,34 @@
+;; -*- coding: utf-8 -*-
+;;
+;; noqlistのテスト
+;; 2014-8-29
+;;
+(use gauche.test)
+(test-start "noqlist")
+
+(use noqlist)
+
+(test-module 'noqlist)
+
+(test-section "noqlist-on")
+(noqlist-on)
+(test* "number-list"  (1 2 3 4 5) (1 2 3 4 5))
+(test* "boolean-list" (#t #f) (#t #f))
+(define x 100)
+(define y 200)
+(test* "symbol-list"  (100 200) (x y))
+(test* "char-list"    (#\a #\b #\space) (#\a #\b #\space))
+(test* "string-list"  ("abc" "de") ("abc" "de"))
+(test* "vector-list"  (#(1 2 3) #(4 5)) (#(1 2 3) #(4 5)))
+
+(test-section "noqlist-off")
+(noqlist-off)
+(test* "number-list"  (test-error <error>) (1 2 3 4 5))
+(test* "boolean-list" (test-error <error>) (#t #f))
+(test* "symbol-list"  (test-error <error>) (x y))
+(test* "char-list"    (test-error <error>) (#\a #\b #\space))
+(test* "string-list"  (test-error <error>) ("abc" "de"))
+(test* "vector-list"  (test-error <error>) (#(1 2 3) #(4 5)))
+
+(test-end)
+
